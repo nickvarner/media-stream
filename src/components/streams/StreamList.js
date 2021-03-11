50a4a8d063bece325fd3dfad0501d3075f3608a5
[nick varner] <nick.varner@gmail.com>
2021-03-11 15:10:27 -0800

    rendered streamlist, admin buttons, and create stream button in streamlist component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
@@ -6,10 +7,21 @@ class StreamList extends React.Component {
 	componentDidMount () {
 		this.props.fetchStreams();
 	}
+	renderAdmin (stream) {
+		if (this.props.currentUserId === stream.userId) {
+			return (
+				<div className='right floated content'>
+					<button className='ui button primary'>edit</button>
+					<button className='ui button negative'>delete</button>
+				</div>
+			);
+		}
+	}
 	renderList () {
 		return this.props.streams.map((stream) => {
 			return (
 				<div className='item' key={stream.id}>
+					{this.renderAdmin(stream)}
 					<i className='large middle aligned icon camera' />
 					<div className='content'>
 						{stream.title}
@@ -19,11 +31,21 @@ class StreamList extends React.Component {
 			);
 		});
 	}
+	renderCreate () {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link to='/streams/new'>create stream</Link>
+				</div>
+			);
+		}
+	}
 	render () {
 		return (
 			<div className='div'>
 				<h2>streams</h2>
 				<div className='ui celled list'>{this.renderList()}</div>
+				{this.renderCreate()}
 			</div>
 		);
 	}
@@ -32,7 +54,11 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
 	//we need to turn the state object back into an array so it can map over
 	//object.values extracts all the values out of an object and turns it into an array
-	return { streams: Object.values(state.streams) };
+	return {
+		streams       : Object.values(state.streams),
+		currentUserId : state.auth.userId,
+		isSignedIn    : state.auth.isSignedIn
+	};
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
