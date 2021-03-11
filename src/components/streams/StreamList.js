e6a3b3799e64aeb53fe14df902eb6d238f5f6d2e
[nick varner] <nick.varner@gmail.com>
2021-03-11 14:22:57 -0800

    hooked up streamreducer

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-export default function StreamList () {
-	return (
-		<div>
-			<h1>StreamList</h1>
-		</div>
-	);
+class StreamList extends React.Component {
+	componentDidMount () {
+		this.props.fetchStreams();
+	}
+	render () {
+		return <div className='streamlist'>streamlist</div>;
+	}
 }
+
+export default connect(null, { fetchStreams })(StreamList);
