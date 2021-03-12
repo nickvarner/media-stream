b235da50624c901385237e22d5a23964233f1df4
[nick varner] <nick.varner@gmail.com>
2021-03-12 13:43:45 -0800

    turned streamlist from class component to functional

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
-class StreamList extends React.Component {
-	componentDidMount () {
-		this.props.fetchStreams();
-	}
-	renderAdmin (stream) {
-		if (this.props.currentUserId === stream.userId) {
+const StreamList = (props) => {
+	React.useEffect(() => {
+		props.fetchStreams();
+	}, []);
+	const renderAdmin = (stream) => {
+		if (props.currentUserId === stream.userId) {
 			return (
 				<div className='right floated content'>
 					<Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
@@ -18,12 +18,12 @@ class StreamList extends React.Component {
 				</div>
 			);
 		}
-	}
-	renderList () {
-		return this.props.streams.map((stream) => {
+	};
+	const renderList = () => {
+		return props.streams.map((stream) => {
 			return (
 				<div className='item' key={stream.id}>
-					{this.renderAdmin(stream)}
+					{renderAdmin(stream)}
 					<i className='large middle aligned icon camera' />
 					<div className='content'>
 						{stream.title}
@@ -32,26 +32,74 @@ class StreamList extends React.Component {
 				</div>
 			);
 		});
-	}
-	renderCreate () {
-		if (this.props.isSignedIn) {
+	};
+	const renderCreate = () => {
+		if (props.isSignedIn) {
 			return (
 				<div style={{ textAlign: 'right' }}>
 					<Link to='/streams/new'>create stream</Link>
 				</div>
 			);
 		}
-	}
-	render () {
-		return (
-			<div className='div'>
-				<h2>streams</h2>
-				<div className='ui celled list'>{this.renderList()}</div>
-				{this.renderCreate()}
-			</div>
-		);
-	}
-}
+	};
+	return (
+		<div className='div'>
+			<h2>streams</h2>
+			<div className='ui celled list'>{renderList()}</div>
+			{renderCreate()}
+		</div>
+	);
+};
+
+// class StreamList extends React.Component {
+// componentDidMount () {
+// 	this.props.fetchStreams();
+// }
+// renderAdmin (stream) {
+// 	if (this.props.currentUserId === stream.userId) {
+// 		return (
+// 			<div className='right floated content'>
+// 				<Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
+// 					edit
+// 				</Link>
+// 				<button className='ui button negative'>delete</button>
+// 			</div>
+// 		);
+// 	}
+// }
+// renderList () {
+// 	return this.props.streams.map((stream) => {
+// 		return (
+// 			<div className='item' key={stream.id}>
+// 				{this.renderAdmin(stream)}
+// 				<i className='large middle aligned icon camera' />
+// 				<div className='content'>
+// 					{stream.title}
+// 					<div className='description'>{stream.description}</div>
+// 				</div>
+// 			</div>
+// 		);
+// 	});
+// }
+// 	renderCreate () {
+// 		if (this.props.isSignedIn) {
+// 			return (
+// 				<div style={{ textAlign: 'right' }}>
+// 					<Link to='/streams/new'>create stream</Link>
+// 				</div>
+// 			);
+// 		}
+// 	}
+// 	render () {
+// 		return (
+// 			<div className='div'>
+// 				<h2>streams</h2>
+// 				<div className='ui celled list'>{this.renderList()}</div>
+// 				{this.renderCreate()}
+// 			</div>
+// 		);
+// 	}
+// }
 
 const mapStateToProps = (state) => {
 	//we need to turn the state object back into an array so it can map over
